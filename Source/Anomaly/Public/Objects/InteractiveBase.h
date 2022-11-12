e601e9968dce01abd061510eb4b6037157e05916
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2022-11-12 05:09:29 +0100

    added an interactive base object

---
+++ Source/Anomaly/Public/Objects/InteractiveBase.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperFlipbookActor.h"
+#include "Engine/CollisionProfile.h"
+#include "InteractiveBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANOMALY_API AInteractiveBase : public APaperFlipbookActor
+{
+	GENERATED_BODY()
+public:
+	AInteractiveBase();
+
+
+	
+};
