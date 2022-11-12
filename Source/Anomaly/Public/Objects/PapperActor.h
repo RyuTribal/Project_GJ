c36d14f077b7c66e85c598f33a704bb6ef0573f4
Mohammed Shakir <mohammedshakir010528@gmail.com>
2022-11-12 05:12:37 +0100

    haram

---
+++ Source/Anomaly/Public/Objects/PapperActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperSpriteActor.h"
+#include "PapperActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANOMALY_API APapperActor : public APaperSpriteActor
+{
+	GENERATED_BODY()
+	
+};
